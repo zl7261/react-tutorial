952b0a527d805615f6d08594f910a002054619fe
杨广 <yangg@yealink.com>
2019-12-19 16:49:21 +0800

    井字棋

--- src/component/Board.tsx
+++ src/component/Board.tsx
@@ -1,29 +1,74 @@
 import React from 'react'
 import {Square} from './Square'
 
-export class Board extends React.Component {
 
+interface BoardState {
+  squares: string[]
+  xIsNext: boolean
+}
 
-  render() {
-    const status = 'Next player: X'
-    const arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+interface BoardProps {
+  squares: string[]
+  onClick: Function
+}
+
+export class Board extends React.Component<BoardProps, BoardState> {
+  constructor(props: BoardProps) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+  renderSquare(i: number) {
     return (
-      <div>
-        <div className="status">{status}</div>
-        {
-          arr.map(row => {
-            return (
-              <div className="board-row">
-                {
-                  row.map(column => {
-                    return <Square value={column}/>
-                  })
-                }
-              </div>
-            )
-          })
-        }
-      </div>
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
     )
   }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
--- src/component/Square.tsx
+++ src/component/Square.tsx
@@ -1,27 +1,23 @@
 import React from 'react'
 
 interface SquareProps {
-  value: number
+  value: string,
+  onClick: Function
 }
+
 interface SquareState {
-  value: number|null|string
+  value: number | null | string
 }
 
 export class Square extends React.Component<SquareProps,SquareState> {
-  constructor(props: SquareProps) {
-    super(props)
-    this.state = {
-      value: null
-    }
-  }
-
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
-    )
+    );
   }
 }
