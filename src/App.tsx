6c4e324995bb5b15d6828e2d4eb20ae38b7d431e
杨广 <yangg@yealink.com>
2019-12-24 10:48:52 +0800

    add img

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,26 @@ import './oxo/Game.css'
 import './component/MyInput'
 import {MyInput} from './component/MyInput'
 import {MyState} from './component/MyState'
+import logo from './logo.svg'
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" style={{height: 200,width:200}}/>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
       <Game/>
       <MyInput/>
       <MyState/>
